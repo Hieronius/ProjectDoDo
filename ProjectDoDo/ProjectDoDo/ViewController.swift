d177e219290876de04eced294210c231d239a055
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-29 18:10:34 +0300

    Add table view with code

--- ProjectDoDo/ProjectDoDo/ViewController.swift
+++ ProjectDoDo/ProjectDoDo/ViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    private var tableView: UITableView = {
+        /// initialization of the tableView
+        let tableView = UITableView.init()
+        tableView.backgroundColor = .orange
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
     }
 
 
