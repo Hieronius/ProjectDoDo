143de84016108efc8e86198293af27613a155508
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-30 08:08:41 +0300

    Create DetailVC

---
+++ ProjectDoDo/ProjectDoDo/Screens/Detail/DetailScreenVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailScreenVC.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 30.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Extension UI and Constraints
+
+extension DetailVC {
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
