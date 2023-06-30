96e97f6c380a91b65f06e7e56152bffe6c08c6ca
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-30 05:20:47 +0300

    Implement custom cell

---
+++ ProjectDoDo/ProjectDoDo/Screens/Menu/CustomCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCell.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 30.06.2023.
+//
+
+import UIKit
+
+final class ProductCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
