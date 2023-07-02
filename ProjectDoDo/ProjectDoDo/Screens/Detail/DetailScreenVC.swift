912d9dcb8066fe07c51e58853920f170a588ee1e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-02 06:25:42 +0300

    refactor collection view initialization process
    And put it into the single function in DetailVC

--- ProjectDoDo/ProjectDoDo/Screens/Detail/DetailScreenVC.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Detail/DetailScreenVC.swift
@@ -10,16 +10,26 @@ import SnapKit
 
 final class DetailVC: UIViewController {
     
+    // MARK: - Private Properties
+    
     private var collectionView: CollectionViewManager!
     
-    private let layout = UICollectionViewLayout()
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        instantiateCollectionView()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func instantiateCollectionView() {
+        let layout = UICollectionViewLayout()
         collectionView = CollectionViewManager(frame: view.bounds, collectionViewLayout: layout)
         view.addSubview(collectionView)
     }
 }
 
-// MARK: - UI and Constraints
+
 
