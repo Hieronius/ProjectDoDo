10f41057ae860d18b9f1a2facfa6eca90cc7d26b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-02 06:33:09 +0300

    Create Ingridient struct

---
+++ ProjectDoDo/ProjectDoDo/App/Models/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  ProductIngridients.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 02.07.2023.
+//
+
+import Foundation
+
+struct Ingredient {
+    var name: String
+    var price: String
+    var image: String
+}
