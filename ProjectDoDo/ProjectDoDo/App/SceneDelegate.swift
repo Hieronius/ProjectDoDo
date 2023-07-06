f8bdb074f08d029d0227e7ae202e202b2099129d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-06 18:52:40 +0300

    Change root VC of the app

--- ProjectDoDo/ProjectDoDo/App/SceneDelegate.swift
+++ ProjectDoDo/ProjectDoDo/App/SceneDelegate.swift
@@ -11,14 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
 
-        let initialVC = MenuScreenVC() as UIViewController
+        let initialVC = DetailScreenVC()
         let navigationController = UINavigationController(rootViewController: initialVC)
         window?.windowScene = windowScene
         self.window?.rootViewController = navigationController
