40cb27596463697ce2fbbb6c97116637531cde21
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-07 06:52:00 +0300

    UITableView + dequeueCell

---
+++ ProjectDoDo/ProjectDoDo/Extensions/UITableView + dequeueCell.swift
@@ -0,0 +1,21 @@
+//
+//  UITableView + dequeueCell.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 07.07.2023.
+//
+
+import UIKit
+
+extension UITableView {
+    func registerCell<Cell: UITableViewCell>(_ cellClass: Cell.Type) {
+        register(cellClass, forCellReuseIdentifier: cellClass.reuseID)
+    }
+    
+    func dequeueCell<Cell: UITableViewCell>(_ indexPath: IndexPath) -> Cell {
+        guard let cell = self.dequeueReusableCell(withIdentifier: Cell.reuseID, for: indexPath) as? Cell
+        else { fatalError("Fatal error for cell at \(indexPath)") }
+        
+        return cell
+    }
+}
