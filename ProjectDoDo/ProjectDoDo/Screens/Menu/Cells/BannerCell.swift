c165f2e2540c810e3e10008ae84a5e76299c6929
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-09 08:22:05 +0300

    Add template of cell component

--- ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/BannerCell.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/BannerCell.swift
@@ -9,4 +9,40 @@ import UIKit
 
 final class BannerCell: UITableViewCell {
     
+    // MARK: - Public Properties
+    
+    static var reuseID = "BannerCell"
+    
+    // MARK: - Private Properties
+    
+    private var container = UIView()
+    private var bannerLable = UILabel()
+    private var bannerCollectionView = BannerCollectionViewManager()
+    
+    // MARK: - Initialization
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupContainer()
+        setupBannerLabel()
+        setupBannerCollectionView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK - Private Methods
+    
+    private func setupContainer() {
+        
+    }
+    
+    private func setupBannerLabel() {
+        
+    }
+    
+    private func setupBannerCollectionView() {
+        
+    }
 }
