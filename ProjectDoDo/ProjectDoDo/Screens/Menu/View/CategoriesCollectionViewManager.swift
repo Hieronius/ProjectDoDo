995638b41867caaf6bdaa5acab788715b6b40407
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-09 07:46:59 +0300

    Create CategoryCollectionManager

--- ProjectDoDo/ProjectDoDo/Screens/Menu/View/CategoriesCollectionViewManager.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Menu/View/CategoriesCollectionViewManager.swift
@@ -5,4 +5,20 @@
 //  Created by Арсентий Халимовский on 09.07.2023.
 //
 
-import Foundation
+import UIKit
+
+final class CategoryCollectionViewManager: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout ) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        setupCollectionView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView() {
+        
+    }
+}
