10f9185800c9593659edb77c30638f9f0cbe0ec1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-10 07:23:20 +0300

    Set BannerManagerCell

--- ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/BannerManagerCell.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/BannerManagerCell.swift
@@ -13,14 +13,64 @@ final class BannerManagerCell: UICollectionViewCell {
     
     static let reuseID = "BannerManagerCell"
     
+    // MARK: Private Properties
+    
+    private var container = UIView()
+    private var productLabel = UILabel()
+    private var priceLabel = UILabel()
+    private var productImage = UIImageView()
+    
+    
     // MARK: Initialization
     
     override init(frame: CGRect) {
         super.init(frame:frame)
+        setupContainer()
+        setupProductLabel()
+        setupPriceLabel()
+        setupProductImage()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+// MARK: Public Methods
+
+extension BannerManagerCell {
+    func update() {
+        
+    }
+}
+
+// MARK: UI and Constraints
+
+extension BannerManagerCell {
+    private func setupContainer() {
+        container.backgroundColor = UIColor.blue
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        container.heightAnchor.constraint(equalToConstant: 0.40 * height).isActive = true
+        container.widthAnchor.constraint(equalToConstant: 0.40 * width).isActive = true
+        contentView.addSubview(container)
+    }
+    
+    private func setupProductLabel() {
+        productLabel.text = "Пицца"
+        productLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        container.addSubview(productLabel)
+    }
+    
+    private func setupPriceLabel() {
+        priceLabel.text = "599"
+        priceLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        container.addSubview(priceLabel)
+    }
+    
+    private func setupProductImage() {
+        productImage.image = UIImage.init(named: "margarita")
+        container.addSubview(productImage)
+    }
 }
