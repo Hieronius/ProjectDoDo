bff20102e1c3d0b9b6b3f659a25997c818f9d93a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-10 07:46:30 +0300

    Configure CategoryManagerCell

--- ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/CategoryManagerCell.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Menu/Cells/CategoryManagerCell.swift
@@ -13,13 +13,47 @@ final class CategoryManagerCell: UICollectionViewCell {
     
     static let reuseID = "CategoryManagerCell"
     
+    // MARK: Private Properties
+    
+    private var container = UIView()
+    private var categoryLabel = UILabel()
+    
     // MARK: Initialization
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupContainer()
+        setupCategoryLabel()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Public Methods
+
+extension CategoryManagerCell {
+    func update() {
+        
+    }
+}
+
+// MARK: UI and Constraints
+
+extension CategoryManagerCell {
+    private func setupContainer() {
+        container.backgroundColor = UIColor.green
+        let height = UIScreen.main.bounds.height
+        let width = UIScreen.main.bounds.width
+        container.heightAnchor.constraint(equalToConstant: 0.40 * height).isActive = true
+        container.widthAnchor.constraint(equalToConstant: 0.40 * width).isActive = true
+        contentView.addSubview(container)
+    }
+    
+    private func setupCategoryLabel() {
+        categoryLabel.text = "Пицца"
+        categoryLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        container.addSubview(categoryLabel)
     }
 }
