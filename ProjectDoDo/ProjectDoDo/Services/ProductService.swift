6d68d4d3f28c29f9c5012c01bafb72abdd121ccb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-11 04:59:06 +0300

    Add more position to product service

--- ProjectDoDo/ProjectDoDo/Services/ProductService.swift
+++ ProjectDoDo/ProjectDoDo/Services/ProductService.swift
@@ -16,6 +16,12 @@ final class ProductService: ProductServiceProtocol {
     let products: [Product] = [
     Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii"),
     Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita"),
+    Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni"),
+    Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii"),
+    Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita"),
+    Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni"),
+    Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii"),
+    Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita"),
     Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni")
     ]
     
