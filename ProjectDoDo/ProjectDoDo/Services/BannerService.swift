2708441e0f2fbd83d2ae5f65f984cd4baf59649e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-13 07:18:19 +0300

    Add products to BannerService

--- ProjectDoDo/ProjectDoDo/Services/BannerService.swift
+++ ProjectDoDo/ProjectDoDo/Services/BannerService.swift
@@ -10,10 +10,12 @@ import Foundation
 final class BannerService {
     
     let products: [Product] = [
-        Product(name: "Гавайская", price: 590, image: "hawaii"),
-        Product(name: "Гавайская", price: 590, image: "hawaii"),
-        Product(name: "Гавайская", price: 590, image: "hawaii"),
-        Product(name: "Гавайская", price: 590, image: "hawaii")
+        Product(name: "Бруслетики", price: 230, image: "Бруслетики"),
+        Product(name: "Пепперони фреш", price: 299, image: "Пепперони фреш"),
+        Product(name: "3 пиццы", price: 1099, image: "3 пиццы"),
+        Product(name: "Додстер", price: 169, image: "Додстер"),
+        Product(name: "Комбо от 599 Р", price: 599, image: "Комбо от 599 Р"),
+        Product(name: "2 соуса", price: 75, image: "2 соуса")
     ]
     
     func fetchProducts() -> [Product] {
