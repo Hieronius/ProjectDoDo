7abd3e54fdcc84cf819c2f3186e718a18ef11b1b
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-18 07:16:15 +0300

    Add Ingredient model

---
+++ ProjectDoDo/ProjectDoDo/Models/Ingredient.swift
@@ -0,0 +1,15 @@
+//
+//  Ingredient.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 18.07.2023.
+//
+
+import Foundation
+
+struct Ingredient {
+    var name: String
+    var price: Int
+    var image: String
+    var type: String
+}
