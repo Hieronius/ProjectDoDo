fe4b81863588e5d7756ffd501c08524adf3933f7
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-18 07:46:07 +0300

    Add counter to Banner and Ingredient Model

--- ProjectDoDo/ProjectDoDo/Models/Banner.swift
+++ ProjectDoDo/ProjectDoDo/Models/Banner.swift
@@ -12,4 +12,5 @@ struct Banner {
     var price: Int
     var image: String
     var type: String
+    var counter: Int
 }
--- ProjectDoDo/ProjectDoDo/Models/Ingredient.swift
+++ ProjectDoDo/ProjectDoDo/Models/Ingredient.swift
@@ -12,4 +12,5 @@ struct Ingredient {
     var price: Int
     var image: String
     var type: String
+    var counter: Int
 }
