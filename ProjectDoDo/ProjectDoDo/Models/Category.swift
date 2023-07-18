685455d503f32a5ce83fee3bf640dcf48077ce9f
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-18 07:17:47 +0300

    Create Category model

---
+++ ProjectDoDo/ProjectDoDo/Models/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 18.07.2023.
+//
+
+import Foundation
+
+struct Category {
+    var name: String
+    var type: String
+}
