335576f647098764f665149d20e2e146a9264fe5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-18 07:17:15 +0300

    Create Banner model

---
+++ ProjectDoDo/ProjectDoDo/Models/Banner.swift
@@ -0,0 +1,15 @@
+//
+//  Banner.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 18.07.2023.
+//
+
+import Foundation
+
+struct Banner {
+    var name: String
+    var price: Int
+    var image: String
+    var type: String
+}
