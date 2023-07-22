48d74ce38b41d2fcf39ff131bf32f14b77e2c734
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-22 06:22:45 +0300

    Add file for TabBar

---
+++ ProjectDoDo/ProjectDoDo/Screens/TabBar.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 22.07.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
