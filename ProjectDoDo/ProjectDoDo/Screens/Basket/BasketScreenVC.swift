c764677d170199938d774ac8a672b3f719ab20cf
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-22 06:30:14 +0300

    add file for BasketScreenVC

---
+++ ProjectDoDo/ProjectDoDo/Screens/Basket/BasketScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  BasketScreenVC.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 22.07.2023.
+//
+
+import UIKit
+
+final class BasketScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
