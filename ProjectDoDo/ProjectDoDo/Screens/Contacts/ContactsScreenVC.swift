f0dfd8e9767424fb962225796fe08a7e0b7d8514
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-22 06:27:20 +0300

    Add file for ContactsScreenVC

---
+++ ProjectDoDo/ProjectDoDo/Screens/Contacts/ContactsScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsScreenVC.swift
+//  ProjectDoDo
+//
+//  Created by Арсентий Халимовский on 22.07.2023.
+//
+
+import UIKit
+
+final class ContactsScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
