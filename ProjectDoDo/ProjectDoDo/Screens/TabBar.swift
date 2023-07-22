fcf1adb262bc2f16ab15bdde2b40b271aa98f9d2
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-22 06:56:20 +0300

    Add controllers to the TabBar

--- ProjectDoDo/ProjectDoDo/Screens/TabBar.swift
+++ ProjectDoDo/ProjectDoDo/Screens/TabBar.swift
@@ -14,4 +14,35 @@ final class TabBarController: UITabBarController {
         
         // some code here
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Make a function for it
+        
+        let menuVC = MenuScreenVC()
+        let menuIcon = UITabBarItem(title: "Меню",
+                                    image: UIImage(named: "someImage"),
+                                    selectedImage: UIImage(named: "otherImage"))
+        menuVC.tabBarItem = menuIcon
+        
+        let profileVC = ProfileScreenVC()
+        let profileIcon = UITabBarItem(title: "Профиль",
+                                       image: UIImage(named: "someImage"),
+                                       selectedImage: UIImage(named: "otherImage"))
+        profileVC.tabBarItem = profileIcon
+        
+        let contactsVC = ContactsScreenVC()
+        let contactsIcon = UITabBarItem(title: "Контакты",
+                                        image: UIImage(named: "someImage"),
+                                        selectedImage: UIImage(named: "otherImage"))
+        
+        let basketVC = BasketScreenVC()
+        let basketIcon = UITabBarItem(title: "Корзина",
+                                      image: UIImage(named: "someImage"),
+                                      selectedImage: UIImage(named: "otherImage"))
+        
+        let controllers = [menuVC, profileVC, contactsVC, basketVC]
+        self.viewControllers = controllers
+    }
 }
