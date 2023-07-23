bf1d0aa468c6b5736915faf9280c38a95d30e612
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-23 08:48:27 +0300

    setup tab bar

--- ProjectDoDo/ProjectDoDo/Screens/TabBar.swift
+++ ProjectDoDo/ProjectDoDo/Screens/TabBar.swift
@@ -9,40 +9,61 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // some code here
+    
+        setupTabBar()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Make a function for it
+        
+    }
+    
+    func setupTabBar() {
+        delegate = self
+        
+        self.tabBar.backgroundColor = .white
         
         let menuVC = MenuScreenVC()
         let menuIcon = UITabBarItem(title: "Меню",
-                                    image: UIImage(named: "someImage"),
-                                    selectedImage: UIImage(named: "otherImage"))
+                                     image: UIImage(systemName: "fork.knife"),
+                                     selectedImage: UIImage(systemName: "fork.knife.fill"))
         menuVC.tabBarItem = menuIcon
         
         let profileVC = ProfileScreenVC()
         let profileIcon = UITabBarItem(title: "Профиль",
-                                       image: UIImage(named: "someImage"),
-                                       selectedImage: UIImage(named: "otherImage"))
+                                       image: UIImage(systemName: "person.crop.circle"),
+                                       selectedImage: UIImage(systemName: "person.crop.circle.fill"))
         profileVC.tabBarItem = profileIcon
         
         let contactsVC = ContactsScreenVC()
         let contactsIcon = UITabBarItem(title: "Контакты",
-                                        image: UIImage(named: "someImage"),
-                                        selectedImage: UIImage(named: "otherImage"))
+                                        image: UIImage(systemName: "map"),
+                                        selectedImage: UIImage(systemName: "map.fill"))
+        contactsVC.tabBarItem = contactsIcon
         
         let basketVC = BasketScreenVC()
         let basketIcon = UITabBarItem(title: "Корзина",
-                                      image: UIImage(named: "someImage"),
-                                      selectedImage: UIImage(named: "otherImage"))
+                                      image: UIImage(systemName: "cart"),
+                                      selectedImage: UIImage(systemName: "cart.fill"))
+        basketVC.tabBarItem = basketIcon
         
         let controllers = [menuVC, profileVC, contactsVC, basketVC]
         self.viewControllers = controllers
+    }
+}
+
+
+
+// MARK: - UITabBarControllerDelegate
+
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        print("Should select viewController: \(viewController.title ?? "") ?")
+        return true
     }
 }
