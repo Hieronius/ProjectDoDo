ca9e0897030b7f519a800d28297f75b02619731e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-26 20:21:40 +0300

    Create outlets for BasketTableCell

--- ProjectDoDo/ProjectDoDo/Screens/Basket/View/Cells/BasketTableCell.swift
+++ ProjectDoDo/ProjectDoDo/Screens/Basket/View/Cells/BasketTableCell.swift
@@ -5,4 +5,39 @@
 //  Created by Арсентий Халимовский on 24.07.2023.
 //
 
-import Foundation
+import UIKit
+
+final class BasketTableCell: UITableViewCell {
+    
+    // MARK: - Public Properties
+    
+    static let reuseID = "BasketCell"
+    
+    // MARK: - Private Properties
+    
+    private var headerLabel = UILabel()
+    private var container = UIView()
+    private var productImage = UIImageView()
+    private var productLabel = UILabel()
+    private var productDetail = UILabel()
+    private var productPrice = UILabel()
+    
+    private var horizontalLine = UIImage()
+    
+    private var counterHorizontalStackView = UIStackView()
+    private var minusButton = UIButton()
+    private var counterLabel = UILabel()
+    private var plusButton = UIButton()
+    
+    // MARK - Initialization
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
